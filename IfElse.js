e0c19e3e123b10e262b1a8114df4d24cd169500c
Rehemamarjan <rehemamarjan50@gmail.com>
2018-02-17 13:35:48 +0300

    added variable_txt.js,IfLess.js

---
+++ IfElse.js
@@ -0,0 +1,8 @@
+var isSunday = false
+if(isSunday){
+	console.log ("watch movie or sleep")
+}
+
+else {console.log ("go to learn code")
+}
+
---
+++ variable_txt.js
@@ -0,0 +1,12 @@
+ var classSize = 23;
+ // a boolian
+ var is weekDay = false;
+ // string
+ var className = "akirachix";
+ // flot
+ var shoeSsize = 6;
+ // 
+
+
+ // this is used to print
+ console.log(classSize)

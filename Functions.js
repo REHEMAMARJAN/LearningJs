b9634a1ae76e608786d7b99f6e1c4d5d926b7eff
Rehemamarjan <rehemamarjan50@gmail.com>
2018-03-03 11:45:12 +0300

    added Function.js

---
+++ Functions.js
@@ -0,0 +1,23 @@
+// void function
+function add(){
+	var numberOne = 8;
+	var numberTwo = 9;
+    var sum = numberOne + numberTwo;
+      console.log(sum);
+     }
+
+    add()
+// Function
+    function multiply(){
+    var numberOne = 3;
+    var numberTwo = 6;
+    var numberThree =8;
+    var answer = numberOne * numberTwo * numberThree;
+
+      return answer
+    }
+     var result = multiply()
+       console.log(result)
+
+
+

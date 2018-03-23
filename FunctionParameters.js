eb4487715d547a197d70d156d98874645a921483
Rehemamarjan <rehemamarjan50@gmail.com>
2018-03-23 16:22:06 +0300

    added FunctionParameters.js

---
+++ FunctionParameters.js
@@ -0,0 +1,19 @@
+   
+   function areaOfTriangle(height,base){
+    var answer = 0.5 * base * height;
+
+      return answer
+}
+   var result = areaOfTriangle(20,14)
+   var node = areaOfTriangle(21,12)
+   var node2 = areaOfTriangle(26,22)
+     
+     print(result)
+     print(node)
+     print(node2)
+
+
+   function print(answer){
+     console.log("Area of triangle"+answer)
+
+   }   

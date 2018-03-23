7f2fa1f674255ef9b26cc9df529f57235db1a7bb
Rehemamarjan <rehemamarjan50@gmail.com>
2018-03-23 16:17:54 +0300

    added Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,8 @@
+var student = ["Jane","Mary","Lauren","Ivy","Fatma","Judith","Irene"]
+var shoeSize = ["7","6","7","5","4","8","3"]
+
+
+
+ for (i=0; i<=student.length; i++) {
+	console.log(student[i]);
+}
